bbcf89b4943a85b9ed95a5510755567fae1a387a
Catherine Angel <catheriner40540@gmail.com>
2025-02-20 22:08:02 +0700

    fix: update main file

--- main.go
+++ main.go
@@ -3,16 +3,16 @@ package main
 import (
 	"fmt"
 
-	// "github.com/catherineeangel/golang-exercise/logic01"
-	// "github.com/catherineeangel/golang-exercise/logic02"
-	"github.com/catherineeangel/golang-exercise/logic03"
+	"github.com/catherineeangel/golang-exercise/logic01"
+	"github.com/catherineeangel/golang-exercise/logic02"
+	// "github.com/catherineeangel/golang-exercise/logic03"
 )
 func main () {
 
 	fmt.Println("Test")
-	// logic01.Sample01()
-	// logic02.Sample02()
-	logic03.Sample03()
+	logic01.Sample01()
+	logic02.Sample02()
+	// logic03.Sample03()
 }
 
 
